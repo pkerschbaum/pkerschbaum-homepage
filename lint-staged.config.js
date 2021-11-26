050667583cee89cc3b2d9e9d5756ea073602ab52
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2021-11-26 19:34:41 +0100

    setup: add prettier, husky, lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '**/*.{js,jsx,ts,tsx}': (filenames) =>
+    `next lint --fix --file ${filenames
+      .map((file) => file.split(process.cwd())[1])
+      .join(' --file ')}`,
+  '**/*': ['prettier --write --ignore-unknown'],
+};
