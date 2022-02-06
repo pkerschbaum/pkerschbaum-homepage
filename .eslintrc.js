f9fa8367361c5715930c7522a433bcb090d03355
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2022-02-06 18:53:39 +0100

    setup: add eslint rules
    setup: add "format" NPM script

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,67 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
+  root: true,
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
+    'plugin:regexp/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:eslint-comments/recommended',
     'next/core-web-vitals',
   ],
-  plugins: ['node'],
+  plugins: ['node', 'regexp'],
   rules: {
     curly: 'error',
     'no-console': 'error',
+    'no-constant-condition': ['error', { checkLoops: false }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector:
+          "MemberExpression[object.name='it'][property.name='only'], MemberExpression[object.name='test'][property.name='only']",
+        message:
+          'Do not check in spec files with tests using ".only" - the other tests of that spec file would be skipped!',
+      },
+      {
+        selector:
+          "MemberExpression[object.name='it'][property.name='skip'], MemberExpression[object.name='test'][property.name='skip']",
+        message: 'Do not check in dead tests. Either fix or delete them.',
+      },
+    ],
+    'no-unneeded-ternary': 'error',
+    'no-useless-computed-key': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error',
+    'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
+    'import/newline-after-import': 'error',
+    'import/no-absolute-path': 'error',
+    'import/no-cycle': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-dynamic-require': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-self-import': 'error',
+    'import/no-useless-path-segments': 'error',
     'import/order': [
       'error',
       {
-        groups: [['builtin', 'external'], 'parent', 'sibling', 'index'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'always-and-inside-groups',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        groups: [['builtin', 'external'], ['parent', 'sibling'], 'index'],
         pathGroups: [
           {
             pattern: '~/**',
             group: 'parent',
           },
         ],
-        pathGroupsExcludedImportTypes: ['builtin'],
-        alphabetize: { order: 'asc', caseInsensitive: true },
-        'newlines-between': 'always-and-inside-groups',
       },
     ],
     'node/no-process-env': 'error',
-    'object-shorthand': 'error',
-    'prefer-template': 'error',
     '@typescript-eslint/ban-types': [
       'error',
       {
@@ -42,11 +73,15 @@ module.exports = {
         extendDefaults: true,
       },
     ],
+    '@typescript-eslint/class-literal-property-style': 'error',
     '@typescript-eslint/explicit-member-accessibility': [
       'error',
       { overrides: { constructors: 'off' } },
     ],
+    '@typescript-eslint/method-signature-style': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-base-to-string': ['error', { ignoredTypeNames: ['Error'] }],
+    '@typescript-eslint/no-confusing-non-null-assertion': 'error',
     '@typescript-eslint/no-floating-promises': 'error',
     '@typescript-eslint/no-meaningless-void-operator': 'error',
     '@typescript-eslint/no-namespace': [
@@ -56,10 +91,19 @@ module.exports = {
         allowDeclarations: true,
       },
     ],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_\\d+' }],
+    '@typescript-eslint/no-require-imports': 'error',
+    '@typescript-eslint/no-throw-literal': 'error',
+    '@typescript-eslint/no-unnecessary-qualifier': 'error',
+    '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    ],
     '@typescript-eslint/non-nullable-type-assertion-style': 'error',
     '@typescript-eslint/prefer-enum-initializers': 'error',
     '@typescript-eslint/prefer-for-of': 'error',
+    '@typescript-eslint/prefer-function-type': 'error',
     '@typescript-eslint/prefer-includes': 'error',
     '@typescript-eslint/prefer-literal-enum-member': 'error',
     '@typescript-eslint/prefer-nullish-coalescing': 'error',
@@ -67,6 +111,8 @@ module.exports = {
     '@typescript-eslint/prefer-readonly': 'error',
     '@typescript-eslint/prefer-reduce-type-parameter': 'error',
     '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+    '@typescript-eslint/promise-function-async': 'error',
     '@typescript-eslint/restrict-template-expressions': [
       'error',
       {
