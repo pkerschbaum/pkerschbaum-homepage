d142c00f4fa45793035d63f874856d1ad4cfcb2f
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2022-05-30 19:41:05 +0200

    design: when scrolling, slide "home" anchor text out and favicons in

--- packages/apps/web/src/components/nav/Nav.tsx
+++ packages/apps/web/src/components/nav/Nav.tsx
@@ -1,14 +1,22 @@
+import Image from 'next/image';
 import type React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-import { CLASSNAME_SCROLLED, HeaderContainer } from '~/components/header';
+import { CLASSNAME_NOT_SCROLLED, CLASSNAME_SCROLLED, HeaderContainer } from '~/components/header';
 import { nameHeadingStyles } from '~/components/introduction';
+import { ColorTheme, DataAttribute } from '~/constants';
 import { Anchor } from '~/elements';
 
 export const Nav: React.FC = () => {
   return (
     <NavContainer>
-      <NavHomeAnchor href="/">Patrick Kerschbaum</NavHomeAnchor>
+      <NavHomeAnchor href="/">
+        <NavHomeAnchorText>Patrick Kerschbaum</NavHomeAnchorText>
+        <NavHomeAnchorLogo>
+          <Image src="/favicons/favicon.svg" height={36} width={36} alt="Logo" />
+          <Image src="/favicons/favicon-dark.svg" height={36} width={36} alt="Logo" />
+        </NavHomeAnchorLogo>
+      </NavHomeAnchor>
 
       <SubNavContainer>
         <NavAnchor href="/blog">Blog</NavAnchor>
@@ -45,17 +53,71 @@ const NavAnchor = styled(Anchor)`
   }
 `;
 
+const slideLeft = keyframes`
+  0% {
+    transform: translateY(0%);
+  }
+  49.9% {
+    margin-inline-start: calc(-1 * (var(--nav-column-gap) + var(--nav-home-anchor-width)));
+  }
+  50% {
+    margin-inline-start: calc(-1 * (var(--nav-column-gap) + var(--nav-home-anchor-width)));
+  }
+  100% {
+    margin-inline-start: calc(-1 * (var(--nav-column-gap) + var(--nav-home-anchor-width)));
+  }
+`;
+const slideRight = keyframes`
+  0% {
+    margin-inline-start: calc(-1 * (var(--nav-column-gap) + var(--nav-home-anchor-width)));
+  }
+  50% {
+    transform: translateY(0%);
+  }
+  100% {
+    transform: translateY(0%);
+  }
+`;
 const NavHomeAnchor = styled(NavAnchor)`
-  --nav-home-anchor-width: 180px;
-  width: var(--nav-home-anchor-width);
+  display: grid;
+  grid-template: 'container';
 
   line-height: 1;
   ${nameHeadingStyles}
 
-  /* animate home anchor out to the left if header is pinned */
-  transition: margin-inline-start 200ms;
-  margin-inline-start: 0px;
+  & > * {
+    grid-area: container;
+
+    width: var(--nav-home-anchor-width);
+
+    animation-name: ${slideRight};
+    animation-duration: 400ms;
+    animation-fill-mode: both;
+  }
+`;
+
+const NavHomeAnchorText = styled.div`
+  --nav-home-anchor-width: 180px;
   ${HeaderContainer}.${CLASSNAME_SCROLLED} & {
-    margin-inline-start: calc(-1 * (var(--nav-column-gap) + var(--nav-home-anchor-width)));
+    animation-name: ${slideLeft};
+  }
+`;
+
+const NavHomeAnchorLogo = styled.div`
+  --nav-home-anchor-width: 40px;
+  display: flex;
+
+  ${HeaderContainer}.${CLASSNAME_NOT_SCROLLED} & {
+    animation-name: ${slideLeft};
+  }
+
+  *:root[${DataAttribute.THEME}='${ColorTheme.DARK}'] && > *:nth-child(1) {
+    display: none !important;
+  }
+  && > *:nth-child(2) {
+    display: none !important;
+  }
+  *:root[${DataAttribute.THEME}='${ColorTheme.DARK}'] && > *:nth-child(2) {
+    display: initial !important;
   }
 `;
