3256cf4d9d973a9e8cba105ae1d8382d8d82c446
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2022-06-12 11:56:44 +0200

    internal: exclude /projects page from production build

--- packages/apps/web/src/pages/projects/index.tsx
+++ packages/apps/web/src/pages/projects/index.tsx
@@ -1,8 +1,10 @@
+import type { GetStaticProps } from 'next';
 import Head from 'next/head';
 import type React from 'react';
 
 import { Main } from '~/components/main';
 import { ProjectsOverview } from '~/components/projects-overview/ProjectsOverview';
+import { config } from '~/config';
 
 const ProjectsPage: React.FC = () => {
   const title = 'Projects';
@@ -24,6 +26,14 @@ const ProjectsPage: React.FC = () => {
       </Main>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = () => {
+  return {
+    props: {},
+    // exclude page from production build
+    notFound: !config.featureFlags.projects,
+  };
 };
 
 export default ProjectsPage;
