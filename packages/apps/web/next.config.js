cb9625a440d20d721e84a0ffef28eb0187aa96c6
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2022-10-30 10:43:51 +0100

    chore(deps): bump all deps to latest

--- packages/apps/web/next.config.js
+++ packages/apps/web/next.config.js
@@ -28,33 +28,6 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
 
-  /* 
-    "puppeteer" has some dependencies on Node.js built-ins (like "fs").
-    We add fallbacks for those plugins so that the application will compile (https://stackoverflow.com/a/70995196/1700319)
-   */
-  future: {
-    webpack5: true,
-  },
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-
-      /* puppeteer-core */
-      fs: false,
-      net: false,
-      readline: false,
-      child_process: false,
-      tls: false,
-
-      /* puppeteer */
-      module: false,
-      'utf-8-validate': false,
-      bufferutil: false,
-    };
-
-    return config;
-  },
-
   async rewrites() {
     /*
      * https://plausible.io/docs/proxy/guides/nextjs#step-1-add-url-rewrite-rules
