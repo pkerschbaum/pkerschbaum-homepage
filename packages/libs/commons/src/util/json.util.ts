bfb55c0d8d4cddc96be63d153e761cc2985e9c43
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2023-01-06 12:51:51 +0100

    chore(deps): bump all deps to latest

--- packages/libs/commons/src/util/json.util.ts
+++ packages/libs/commons/src/util/json.util.ts
@@ -1,6 +1,5 @@
-// @ts-expect-error -- types of "safe-stable-stringify" do not work with "module" set to "node16" in tsconfig.json
 import safeStringify from 'safe-stable-stringify';
 
 export const jsonUtil = {
-  safeStringify: safeStringify as typeof JSON.stringify,
+  safeStringify,
 };
