4a3cd18a9b435eca676c05b6d9ed4e85b30fe10a
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2024-12-25 13:48:45 +0100

    feat: new blog post `large-scale-code-changes-using-asts-and-jscodeshift` (#25)

---
+++ apps/web/src/app/blog/large-scale-code-changes-using-asts-and-jscodeshift/mdx-content-client-component.tsx
@@ -0,0 +1,6 @@
+'use client';
+import MDXContent from '#pkg/writing/posts/large-scale-code-changes-using-asts-and-jscodeshift.mdx';
+
+export function MDXContentClientComponent() {
+  return <MDXContent />;
+}
