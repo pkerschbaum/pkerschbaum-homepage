68bb569ab4c20b8f0d5b66b319e24d3513c54321
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2025-03-15 10:49:35 +0100

    chore: remove Sentry for now (not really used currently)

--- platform/config-eslint/eslint-ecma.cjs
+++ platform/config-eslint/eslint-ecma.cjs
@@ -32,7 +32,7 @@ module.exports = {
     'plugin:eslint-comments/recommended',
     'prettier',
   ],
-  ignorePatterns: ['.eslintrc.cjs', 'dist/**/*', 'sentry.*.config.js'],
+  ignorePatterns: ['.eslintrc.cjs', 'dist/**/*'],
   rules: {
     curly: 'error',
     'multiline-comment-style': ['error', 'starred-block'],
