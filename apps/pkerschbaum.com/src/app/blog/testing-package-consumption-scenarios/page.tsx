4d43dbfce4e9bc677343be78c9b6bb2f415c0dd3
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2025-04-19 20:17:28 +0200

    feat: blog post `testing-package-consumption-scenarios` (#29)

---
+++ apps/pkerschbaum.com/src/app/blog/testing-package-consumption-scenarios/page.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from 'next';
+import path from 'path';
+import type React from 'react';
+import invariant from 'tiny-invariant';
+
+import { MDXContentClientComponent } from '#pkg/app/blog/testing-package-consumption-scenarios/mdx-content-client-component.jsx';
+import styles from '#pkg/app/blog/testing-package-consumption-scenarios/styles.module.css';
+import { ArticleContainerBlogPost } from '#pkg/components/article-container-blog-post/index.js';
+import { ClassesAliases } from '#pkg/constants-browser.js';
+import { PATHS } from '#pkg/constants-server.js';
+import { mapMDXParseResultToMetadata, parseMDXFileAndCollectHrefs } from '#pkg/mdx/index.js';
+
+const faviconsClassName = styles[ClassesAliases.FAVICONS];
+
+const SEGMENT = path.parse(__dirname).name;
+
+async function BlogPostPage() {
+  invariant(faviconsClassName);
+
+  const mdxParseResult = await parseMDXFileAndCollectHrefs(
+    path.join(PATHS.POSTS, `${SEGMENT}.mdx`),
+  );
+
+  return (
+    <ArticleContainerBlogPost
+      mdxContent={<MDXContentClientComponent />}
+      mdxParseResult={mdxParseResult}
+      faviconsClassName={faviconsClassName}
+    />
+  );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const mdxParseResult = await parseMDXFileAndCollectHrefs(
+    path.join(PATHS.POSTS, `${SEGMENT}.mdx`),
+  );
+
+  return mapMDXParseResultToMetadata(mdxParseResult);
+}
+
+export default BlogPostPage;
+
+export { BLOG_REFETCH_INTERVAL_SECONDS as revalidate } from '#pkg/constants-server.js';
